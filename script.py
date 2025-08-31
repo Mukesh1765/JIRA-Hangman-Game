845656abdc4bc68e7f94ea4b9a97f2a00a91d253
Mukesh1765 <161288818+Mukesh1765@users.noreply.github.com>
2025-08-31 17:21:08 +0530

    HT-18 finish main logic

--- script.py
+++ script.py
@@ -56,7 +56,7 @@ class Hangman:
         self.__country_name = self.__get_country_name(self.__get_random_country_names('./countries.csv')).lower()
         self.__guessed_letters = set()
         self.__max_attempts = 5
-        self.__char_array = ['_' for _ in self.__country_name]
+        self.__char_array = [char if char == ' ' else '_' for char in self.__country_name]
         self.__attempts = 0
 
     def start_game(self):
@@ -85,16 +85,50 @@ _ _
 
         print()
         print()
-
-        print("Lets start the game!")
-        print(f"The country name has {len(self.__country_name)} letters.")
-        print()
-
         self.__game_logic()
 
 
     def __game_logic(self):
-        print(" ".join(self.__char_array))
+        print("Lets start the game!")
+        country_len = len(self.__country_name) - self.__country_name.count(' ')
+        print(f"The country name has {country_len} letters.")
+        print()
+        print(' '.join(self.__char_array))
+        country_name_list = [char for char in self.__country_name]
+
+        while self.__char_array != country_name_list and self.__attempts < self.__max_attempts:
+            guess = input("Enter a Letter: ").lower()
+            # if len(guess) != 1 or not guess.isalpha():
+            #     print("Invalid input. Please enter a single letter.")
+            #     continue
+
+            if guess in self.__guessed_letters:
+                print("You've already guessed that letter.")
+                continue
+
+            self.__guessed_letters.add(guess)
+
+            if guess in self.__country_name:
+                print("Good guess!")
+                for i, letter in enumerate(self.__country_name):
+                    if letter == guess:
+                        self.__char_array[i] = guess
+            else:
+                print("Wrong guess.")
+                self.__attempts += 1
+
+            print("current state: ")
+            print(self.hangman_symbols[self.__attempts])
+            print(f"Attempts left: {self.__max_attempts - self.__attempts}")
+            print(" ".join(self.__char_array))
+            print()
+
+        print("Game Over!")
+        if self.__char_array == country_name_list:
+            print("Congratulations! You've guessed the country name correctly!")
+        else:
+            print("Sorry, you've run out of attempts.")
+        print(f"The country was: {self.__country_name}")
 
     @staticmethod
     def __get_random_country_names(file_path):
